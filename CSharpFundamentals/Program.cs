04d242e91129b4e98d30f856cfdd4f7fbfbe4685
SamirAhma <ahmaddin1000@gmail.com>
2024-02-15 20:49:17 +0800

    1

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 10;
+            Console.WriteLine(array1[0]);
+            Console.WriteLine(array2[0]);
+        }
+    } 
+}
+
